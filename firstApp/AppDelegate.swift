5cb096520e5c84cd7d074e5b2e04cf03f7b3993c
Sriram <dukeonline98@outlook.com>
2019-06-05 16:39:14 +0530

    Initial Commit

---
+++ firstApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  firstApp
+//
+//  Created by Sriram on 05/06/19.
+//  Copyright © 2019 sriramph. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ firstApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Sriram on 05/06/19.
+//  Copyright © 2019 sriramph. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
